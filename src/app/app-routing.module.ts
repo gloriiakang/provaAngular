55be45b8044409a6c57266abad28f0d40089d367
gloriiakang <lori.vihchoi@gmail.com>
2021-02-10 17:53:58 -0300

    Prova Angular Gloria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CardsComponent } from './pages/cards/cards.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { FormsComponent } from './pages/forms/forms.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent},
+  { path: 'cards', component: CardsComponent},
+  { path: 'pricing', component: PricingComponent},
+  { path: 'blocks', component: BlocksComponent},
+  { path: 'forms', component: FormsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { FormsComponent } from './pages/forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    DashboardComponent,
+    CardsComponent,
+    PricingComponent,
+    BlocksComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
